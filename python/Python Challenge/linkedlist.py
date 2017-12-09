c4b385a584c936d5160fc3177e06e74d863d3327
WitheredGryphon <WitheredGryphn@gmail.com>
2017-12-09 05:31:04 -0600

    Properly finished linked

--- python/Python Challenge/linkedlist.py
+++ python/Python Challenge/linkedlist.py
@@ -11,11 +11,14 @@ word_list = text.split()
 for i in range(0, 450):
 	if i == 0:
 		linked_word_list = word_list
-	str = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + linked_word_list[-1]
-	print(str)
-	linked_req = urllib.request.Request(str)
+	url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + linked_word_list[-1]
+	print(url)
+	linked_req = urllib.request.Request(url)
 	linked_resp = urllib.request.urlopen(linked_req)
 	linked_html = linked_resp.read()
 	linked_text = linked_html.decode()
+	prev_number = int(linked_word_list[-1])
 	linked_word_list = linked_text.split()
-	
+	if (linked_word_list[0] == "Yes."):
+		prev_number /= 2
+		linked_word_list = str(prev_number)
