8269d3356d71c2bb94772d0b25ecf9b7b9c9585e
WitheredGryphon <WitheredGryphon@gmail.com>
2018-05-18 10:00:27 -0500

    Trying out responsive connected gallery

--- sites/vn_temp/js/jcarousel.connected-carousels.js
+++ sites/vn_temp/js/jcarousel.connected-carousels.js
@@ -34,6 +34,43 @@
                     carousel: carouselStage
                 });
         });
+		
+		var jcarousel = $('.jcarousel');
+
+		jcarousel
+			.on('jcarousel:reload jcarousel:create', function () {
+				var carousel = $(this),
+					width = carousel.innerWidth();
+
+				if (width >= 1700) {
+					width = width / 10;
+				} else if (width >= 1500) {
+					width = width / 9;
+				}
+				else if (width >= 1350) {
+					width = width / 8;
+				}
+				else if (width >= 1200) {
+					width = width / 7;
+				}
+				else if (width >= 1050) {
+					width = width / 6;
+				}
+				else if (width >= 900) {
+					width = width / 5;
+				}
+				else if (width >= 750) {
+					width = width / 4;
+				}
+				else if (width >= 540) {
+					width = width / 3;
+				}
+				else if (width >= 330) {
+					width = width / 2;
+				}
+
+				carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+			})
 
         // Setup controls for the stage carousel
         $('.prev-stage')
